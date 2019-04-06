09b3888168432d1e7d9b10ab5b597f3fab1ecdb0
JR-Tech-and-Software <49214876+JR-Tech-and-Software@users.noreply.github.com>
2019-04-05 22:13:59 -0500

    Update redctl.py

--- redctl.py
+++ redctl.py
@@ -1,6 +1,10 @@
-#from tkinter import ttk
+# The python redctl file contains a graphic frontend to the redctl script. This was my first Tk project.
+
+# Libraries Needed
 from tkinter import Tk, StringVar, Radiobutton, Label, Button
 from os import system
+
+# Function to figure out what to execute depending on what entered
 def execu():
     '''
     print("Execute Called")
@@ -32,13 +36,21 @@ def execu():
     else:
         statuslabel.configure(text="Unexpected Error. Closing in 5 seconds...")
         statuslabel.after(5000, func=dest)
+
+# Close
 def dest():
     root.destroy()
+
+# Select
 def select():
     print(var.get())
+    
+# Get the window ready
 root = Tk()
 root.title("Redctl Tk Frontend")
 var = StringVar()
+
+# Interface
 Label(root, text="Welcome to the Redctl Graphic Frontend!!!").grid(row=0, column=5, padx=5, pady=5)
 Radiobutton(root, text="Off", variable=var, value=1).grid(row=1, column=1)
 Radiobutton(root, text="Level 1", variable=var, value=2).grid(row=2, column=1)
@@ -48,6 +60,8 @@ Radiobutton(root, text="Level 4", variable=var, value=5).grid(row=5, column=1)
 Radiobutton(root, text="Level 5", variable=var, value=6).grid(row=6, column=1)
 Radiobutton(root, text="Level 6", variable=var, value=7).grid(row=7, column=1)
 Radiobutton(root, text="Level 7", variable=var, value=8).grid(row=8, column=1)
+
+# Labels for spacing
 Label(root, text=" ").grid(row=0, column=0)
 Label(root, text=" ").grid(row=0, column=6)
 Label(root, text=" ").grid(row=0, column=2)
@@ -62,7 +76,13 @@ Label(root, text=" ").grid(row=0, column=12)
 Label(root, text=" ").grid(row=0, column=13)
 Label(root, text=" ").grid(row=0, column=14)
 Label(root, text=" ").grid(row=0, column=15)
+
+# Create a status lable
 statuslabel = Label(root, text="")
 statuslabel.grid(row=9, column=5)
+
+# Execute button
 Button(root, text="Execute", command=execu).grid(row=10, column=5, padx=5, pady=5)
+
+# Loop
 root.mainloop()
